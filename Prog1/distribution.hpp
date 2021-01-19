3f09ab052d0c3a9f9de050e700743c1606161e9c
Justin Greever <jgreever@gmail.com>
2021-01-18 20:21:11 -0800

    got most comments setup for the files and added some more code for adding/editing items/inventory

--- Prog1/distribution.hpp
+++ Prog1/distribution.hpp
@@ -8,9 +8,18 @@ class distribution : public warehouse
     public:
         distribution();
         ~distribution();
-        void set_transport();
-        void choose_speed();
-        void change_speed();
+
+        //This will let us check the speed that the user choose for
+        //delivery and ensure we match the fastest or best route to them.
+        void check_speed();
+
+        //This will allow us to check if we have ground or air transport
+        //for a specific package/item being shipped.
+        void check_transport();
+
+        //This will allow us to hold a package in the event a person cancels
+        //their order or needs it held for pickup at a local distribution center
+        void hold_package(int tracking_num);
 
     protected:
         int transportation_type; //0 = ground, 1 = air
--- Prog1/orders.hpp
+++ Prog1/orders.hpp
@@ -3,34 +3,73 @@
 
 #include "warehouse.hpp"
 
+//MAX categories a warehouse can have for products, to keep it simple.
+static const int SIZE=10;
 
+//Our product class will manage the physical item that is being shipped.
+//It will hold the item name, product number, tracking ID, and the current
+//stage of where it is in the chain, as well as the previous places it has
+//been.
 class product
 {
     public:
+        //This is our default constructor
         product();
-        product(const char *name, char *number, int tracking);
+
+        //This is a copy constructor
+        product(const char *name, char *number);
+        
+        //This is another copy constructor
         product(const product &);
+        
+        //This is our default destructor
         ~product();
-        void add_product();
-        void edit_product();
-        void add_tracking();
-        void check_inventory();
+        
+        //add_product will take in the product name and the product number
+        //and create an object out of them.
+        void add_product(char *name, char *number);
+
+        //edit_product allows us to change the product name or number
+        void edit_product(product &aproduct);
+
+        //add_tracking allows us to create a tracking number for the package
+        //and to track it's current place in the shipping chain. If void is
+        //used in place of tracking, it will only update the current place
+        //in the chain and leave tracking alone.
+        void add_tracking(int tracking, int current);
+
+        //In the check_inventory function, we will be passing in either the
+        //product name, or the product number, then a 0 or 1, with 0 = name, 
+        //and 1 = number.
+        void check_inventory(char *check_product, int search_what);
 
     protected:
         char *item_name;
         char *product_number;
         int tracking_id;
-        int current_stage;
+        int *current_stage;
 };
 
-class node : public product
+//The inventory class will manage the items in each warehouse via a categories.
+//It will use an array of category names (max size of 100), that points to a 
+//LLL that contains information about each product in that category such as
+//the item name and item number.
+class inventory : public product
 {
     public:
-        node(const char *aname, char *anumber, int atracking);
-        node(const product &);
+        inventory(const char *aname, char *anumber);
+        inventory(const product &);
+        void search_inventory(char *aname, char *anumber);
+        void edit_inventory(product &aproduct);
+        void add_inventory(char *aname, char *anumber);
+
     protected:
-        //int current_stage; //I *don't* think I will need this here
-        node *next;
+        //the char *categories[SIZE] below will most likely be removed and 
+        //changed to a constructor that creates 10 categories for the warehouse
+        //and then points to a LLL for each item in that category.
+        char *categories[SIZE];
+        product *aproduct;
+        inventory *next;
 };
 
 //This is our orders class. It will be in charge of adding the needed
@@ -59,6 +98,11 @@ class orders : public product
         //items separate from each other. It helps me to work with
         //dynamic memory and what not. :D
         void add_address(char *street, char *city, char *state, char *zip);
+
+        //This allows us to choose how we want the item shipped to us.
+        //We can choose from Overnight, Express, or Standard. I'm 
+        //thinking there may be a better way to tie in the shipping
+        //in the near future before the code is fully turned in.
         void choose_shipping();
 
     protected:
