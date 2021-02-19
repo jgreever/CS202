03db448b18148fe3abd9fcd7fa64f148bbf3b83d
Justin Greever <jgreever@gmail.com>
2021-02-18 17:20:30 -0800

    A bit of tinkering with node/bst header files and main.cpp comments

---
+++ Prog3/main.cpp
@@ -0,0 +1,38 @@
+/* main.cpp */
+
+/* Justin Greever
+ * CS202 - Program 3
+ * 2/18/2021
+ *
+ * This program is designed to teach us about operator overloading
+ * and inheritance better than we already should.
+ *
+ * This program will have us create a "contact list" that holds info
+ * about a contact and three methods of contact (two paid, one must
+ * be a text based option) for each contact. In this case, mine will
+ * be a cellphone, a pager (who uses these even?! xD) and a VoIP type
+ * service, similar to Skype Premium.
+ *
+ * Operator Overloading Requirements (as per the CS202 Program 3.pdf):
+ * "The operators to support must include: =, +, +=, [], ==, !=,
+ *  the relational/equality operators, and the ability to input/output
+ *  data."
+ *
+ * "In Program #3, you CAN now write your own STRING data type, but it
+ *  can’t be the only place you use operator overloading (since it is
+ *  provided in topic #6!)"
+ *
+ * The Data Structure I will be using will be a BST and a LLL. Both of
+ * these will be implemented as classes, no structs, to keep it OOP.
+ *
+ */
+
+#include "node.hpp"
+
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
